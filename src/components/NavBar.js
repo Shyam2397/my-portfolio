5d5b02f0a3a38be60dff4a9bc57c66179f3f8b4f
Shyam sundar R <shyamsundarr10@gmail.com>
2023-08-07 14:27:43 +0530

    the nav bar is created and is fixed

---
+++ src/components/NavBar.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+    },
+    {
+      id: 2,
+      link: "About",
+    },
+    {
+      id: 3,
+      link: "Skills",
+    },
+    {
+      id: 4,
+      link: "Projects",
+    },
+    {
+      id: 5,
+      link: "Contact",
+    },
+  ];
+
+  return (
+    <div className="flex justify-between items-center w-full h-20 bg-black text-white fixed px-4">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Shyam</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="pr-4 z-10 cursor-pointer text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="text-4xl px-4 py-6 cursor-pointer capitalize"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
