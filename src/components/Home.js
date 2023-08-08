338461de7bd53aea9dea5b2d9857de6d618aadf1
Shyam sundar R <shyamsundarr10@gmail.com>
2023-08-08 17:39:56 +0530

    home page is updated with my name

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,38 @@ import React from "react";
 import { RiArrowRightSLine } from "react-icons/ri";
 import profile from "../assets/profile.jpeg";
 import { Link } from "react-scroll";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ["Full Stack Developer..", "Professional Coder.."],
+    loop: true,
+    typeSpeed: 20,
+    deleteSpeed: 10,
+    delaySpeed: 2000,
+  });
+
   return (
     <div
       name="Home"
-      className="h-full w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="flex flex-col w-full h-full md:pt-20 bg-gradient-to-t from-black to-gray-800 lg:flex-row max-[426px]:px-5"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full p-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full mt-36 max-[426px]:mt-28 md:mt-30 lg:mt-24">
-          <h2 className="text-4xl sm:text-7xl md:text-5xl lg:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+      <div className="grid flex-grow h-screen lg:ml-40  place-items-center">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-white">
+            Hi, I'm{" "}
+            <span className="lg:text-7xl text-red-700 capitalize">
+              shyam sundar
+            </span>
           </h2>
-          <p className="py-4 text-gray-300 max-w-md">
-            Seeking Entry-level positions in a growth-oriented company in any
-            field. To acquire a challenging position in a reputed organization
-            where I could use my hardworking and creative skills for the growth
-            and good market position of the company.
-          </p>
+          <h2 className="text-gray-300 font-bold lg:text-4xl text-3xl max-w-md">
+            a <span>{text}</span>
+            <Cursor
+              cursorBlinking="false"
+              cursorStyle="|"
+              cursorColor="#ff014f"
+            />
+          </h2>
           <div>
             <Link
               to="Projects"
@@ -34,11 +48,13 @@ const Home = () => {
             </Link>
           </div>
         </div>
-        <div className="mt-5 md:mt-12 lg:mt-16">
+      </div>
+      <div className="grid flex-grow h-screen place-items-center">
+        <div>
           <img
             src={profile}
             alt="my profile"
-            className="mx-auto w-1/2 rounded-3xl md:w-1/2"
+            className="mx-auto rounded-3xl w-10/12 lg:w-5/12 md:w-6/12 sm:w-9/12"
           />
         </div>
       </div>
