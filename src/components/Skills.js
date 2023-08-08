03caad164db0a865960c6f9783c1206fb707cfdf
Shyam sundar R <shyamsundarr10@gmail.com>
2023-08-08 14:21:00 +0530

    import has updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,12 @@
 import React from "react";
-import html from "../assets/html.png";
-import css from "../assets/css.png";
-import github from "../assets/github.png";
-import graphql from "../assets/graphql.png";
-import javascript from "../assets/javascript.png";
-import nextjs from "../assets/nextjs.png";
-import react from "../assets/react.png";
-import tailwind from "../assets/tailwind.png";
+import html from "../assets/skills/html.png";
+import css from "../assets/skills/css.png";
+import github from "../assets/skills/github.png";
+import graphql from "../assets/skills/graphql.png";
+import javascript from "../assets/skills/javascript.png";
+import nextjs from "../assets/skills/nextjs.png";
+import react from "../assets/skills/react.png";
+import tailwind from "../assets/skills/tailwind.png";
 
 const Skills = () => {
   const skills = [
