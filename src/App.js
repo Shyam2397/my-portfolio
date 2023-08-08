cc9c825055b1d246e266e22fad357e7270a55f1c
Shyam sundar R <shyamsundarr10@gmail.com>
2023-08-08 14:14:52 +0530

    pages routes has commited

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import SocialLink from "./components/SocialLink";
+
 function App() {
   return (
-     <div>
-      hello
-     </div> 
-    );
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+
+      <SocialLink />
+    </div>
+  );
 }
 
 export default App;
